17628d4e558533ed6ec3607182f34161fe3002fd
kunal <kunal@Kunals-MacBook-Air.local>
2026-01-09 15:58:58 +0530

    update backend.tf

--- backend.tf
+++ backend.tf
@@ -3,7 +3,9 @@ terraform {
     bucket = "statefile-12345"
     key    = "statebackendfile"
     region = "us-east-1"
+    encrypt = true
   }
+  required_version = ">=0.13.0"
   required_providers {
     aws = {
       source  = "hashicorp/aws"
